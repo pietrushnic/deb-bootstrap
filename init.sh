9dbf2c25a28a00af129e977b219c5206fef84f17
Piotr Król <piotr.krol@3mdeb.com>
2014-10-05 03:14:39 +0200

    bootstrap: try sed for all pp files

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo puppet module install puppetlabs-vcsrepo
 sudo puppet module install saz-sudo
+sed -i.bak "s/\$id/${USER}/" $HOME/src/deb-bootstrap/*.pp
 sudo puppet apply $HOME/src/deb-bootstrap/sys-conf.pp
-sed -i.bak "s/\$id/${USER}/" $HOME/src/deb-bootstrap/user-conf.pp
 sudo puppet apply $HOME/src/deb-bootstrap/user-conf.pp
