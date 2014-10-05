fadd8a4ee226bbc2fa12cdf12ea0084a1a7410c9
Piotr Król <piotr.krol@3mdeb.com>
2014-10-05 03:04:35 +0200

    bootstrap: fix  replacemnet by sed

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 sudo puppet module install puppetlabs-vcsrepo
 sudo puppet module install saz-sudo
 sudo puppet apply $HOME/src/deb-bootstrap/sys-conf.pp
-cat $HOME/src/deb-bootstrap/user-conf.pp | sed 's/$id/${USER}/' > $HOME/src/deb-bootstrap/user-conf.pp
+sed "s/\$id/${USER}/" $HOME/src/deb-bootstrap/user-conf.pp
 sudo puppet apply $HOME/src/deb-bootstrap/user-conf.pp
