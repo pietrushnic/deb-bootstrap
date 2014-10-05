2a55e8d74c205622c86f4b1198d9d08788152b6a
Piotr Król <piotr.krol@3mdeb.com>
2014-10-05 03:10:05 +0200

    bootstrap: use -i to read and write to the same file

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 sudo puppet module install puppetlabs-vcsrepo
 sudo puppet module install saz-sudo
 sudo puppet apply $HOME/src/deb-bootstrap/sys-conf.pp
-sed "s/\$id/${USER}/" $HOME/src/deb-bootstrap/user-conf.pp
+sed -i.bak "s/\$id/${USER}/" $HOME/src/deb-bootstrap/user-conf.pp
 sudo puppet apply $HOME/src/deb-bootstrap/user-conf.pp
