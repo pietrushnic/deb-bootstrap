377f5ecbbe42789b3f6b23c199c3c146b63c0a48
Piotr Król <piotr.krol@3mdeb.com>
2014-10-05 02:59:59 +0200

    boostrap: use init.sh with ability to replace username in user-conf.pp

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo puppet module install puppetlabs-vcsrepo
+sudo puppet module install saz-sudo
+sudo puppet apply $HOME/src/deb-bootstrap/sys-conf.pp
+cat $HOME/src/deb-bootstrap/user-conf.pp | sed 's/$id/${USER}/' > $HOME/src/deb-bootstrap/user-conf.pp
+sudo puppet apply $HOME/src/deb-bootstrap/user-conf.pp
